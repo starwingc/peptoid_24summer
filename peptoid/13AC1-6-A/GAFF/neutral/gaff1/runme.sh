4efd5b6c006fdfcbadbb1c5ddda3a15b1458a2bc
starwingc <tur54445@temple.edu>
2024-07-03 19:40:59 -0400

    correct solvent box for 13AC1-6-A

--- peptoid/13AC1-6-A/GAFF/neutral/gaff1/runme.sh
+++ peptoid/13AC1-6-A/GAFF/neutral/gaff1/runme.sh
@@ -45,13 +45,12 @@ cat >> 13AC1-6-A_sol.top <<EOL
 EOL
 
 # adding the following two atomtypes in peptoid.itp:
-cat >> 13AC1-6-A.itp <<EOL
+#cat >> 13AC1-6-A.itp <<EOL
 
-[atomtypes]
- cl       cl          0.00000  0.00000   A     3.46595e-01   1.10374e+00 ; 1.95  0.2638
- h3       h3          0.00000  0.00000   A     2.06564e-01   8.70272e-02 ; 1.16  0.0208 
+#[atomtypes]
 
-EOL
+
+#EOL
 
 
 # Make an index file for our solvated+neutralized  peptoid
@@ -65,7 +64,7 @@ gmx grompp -f mdp/em.mdp -c 13AC1-6-A_sol.gro -p 13AC1-6-A_sol.top -n index.ndx 
 
 #Run the EM 
 gmx mdrun -nt 1 -v -s em_sol.tpr -c em_sol.gro -o em_sol.trr
-# 3.6788292e+01 on atom 5972
+# 7.7511566e+01 on atom 43
 
 # Prepare a NVT.tpr file
 gmx grompp -f mdp/nvt.mdp -c em_sol.gro -p 13AC1-6-A_sol.top -n index.ndx -o nvt_sol.tpr -maxwarn 4
@@ -86,5 +85,5 @@ gmx mdrun -nt 1 -v -s npt_sol.tpr -c npt_sol.gro -o npt_sol.trr
 echo "18\n0\n" |gmx energy -f ener.edr -o temperature.xvg
 
 # Prepare for productive run 
-gmx grompp -f mdp/prod_1.mdp -c npt_sol.gro -t state.cpt -p 13AC1-6-A_sol.top -o prod_1.tpr -maxwarn 4
-gmx grompp -f mdp/prod_2.mdp -c npt_sol.gro -t state.cpt -p 13AC1-6-A_sol.top -o prod_2.tpr -maxwarn 4
+# gmx grompp -f mdp/prod_1.mdp -c npt_sol.gro -p 13AC1-6-A_sol.top -o prod_1.tpr -maxwarn 4
+gmx grompp -f mdp/prod_2.mdp -c npt_sol.gro -p 13AC1-6-A_sol.top -o prod_2.tpr -maxwarn 4
