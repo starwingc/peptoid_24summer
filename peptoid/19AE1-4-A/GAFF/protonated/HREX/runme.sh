8f5e1b54a2f2809b6cf3b5cbdc57f1ba536affee
starwingc <tur54445@owlsnest.hpc.temple.edu>
2024-07-04 13:48:52 -0400

    facing initial thermodynamic state 5 does not exist, only goes to 0 in mdp filefor 19AE1-4-A

---
+++ peptoid/19AE1-4-A/GAFF/protonated/HREX/runme.sh
@@ -0,0 +1,11 @@
+# Transfer the file name
+# Prepare n state with different FEP lambda value
+vi prepare_tpr.sh
+
+# Activate the prepare_tpr.sh and run
+chmod +x prepare_tpr.sh
+./prepare_tpr.sh
+# The result should be state_n/ include all the file and tpr for later running
+
+ 
+
